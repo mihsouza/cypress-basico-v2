b6ac0bbbe8966b327fb3d93d5371171409cd0658
michele.souza <michele-leticia_souza@hotmail.com>
2022-04-09 14:40:15 -0300

    Ajuste no teste para verificar o título da página "Política de privacidade"

--- cypress/integration/Privacy.spec.js
+++ cypress/integration/Privacy.spec.js
@@ -2,5 +2,5 @@ beforeEach(function(){
     cy.visit('./src/privacy.html')
 })
 it('verifica o título da página de privacidade', function() {
-    cy.title().should('be.equal', 'CAC TAT - Política de privacidade')
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT - Política de privacidade')
 })
